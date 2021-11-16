9668fa93f5db4b3c6219a8f1086c7491e0b57a7f
kakkki <kakki4062@gmail.com>
2021-11-16 17:39:03 +0900

    Initial Commit

---
+++ swiftui-simple-tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-simple-tutorial
+//
+//  Created by Atsuki Kakehi on 2021/11/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-simple-tutorial/swiftui_simple_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_simple_tutorialApp.swift
+//  swiftui-simple-tutorial
+//
+//  Created by Atsuki Kakehi on 2021/11/16.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_simple_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
