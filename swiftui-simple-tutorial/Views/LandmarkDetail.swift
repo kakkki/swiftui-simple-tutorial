6cbadbd69b15beb5c8772e7edd4fc436d2158b56
kakkki <kakki4062@gmail.com>
2021-11-16 18:56:54 +0900

    implement LandmarkDetail

--- swiftui-simple-tutorial/Views/LandmarkDetail.swift
+++ swiftui-simple-tutorial/Views/LandmarkDetail.swift
@@ -8,13 +8,50 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
+    var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            CircleImage(image: landmark.image)
+                .offset(y: 0)
+                .padding(.bottom, 10)
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                Divider()
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                    .padding(.bottom, 2)
+                Text(landmark.description)
+            }
+            .padding(.leading)
+            .padding(.trailing)
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
+
     static var previews: some View {
-        LandmarkDetail()
+        LandmarkDetail(landmark: modelData.landmarks[0])
+            .environmentObject(modelData)
     }
 }
